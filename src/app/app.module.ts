ac1e6c9764323a46afee76fbc0e12da061763e0e
Daniel Blasko <blaskoi@gmail.com>
2018-08-21 19:19:23 +0200

    Set docksize  on landingpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
@@ -14,6 +16,7 @@ import { CardComponent } from './components/card/card.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -7,10 +7,28 @@ import { Settings } from "../../services/settings.service";
   styleUrls: ['./landingpage.component.scss']
 })
 export class LandingpageComponent implements OnInit {
+  dockSize: number[];
+  docknumber: number;
 
-  constructor(private appSettings: Settings) { }
+  constructor(private appSettings: Settings) {}
 
   ngOnInit() {
+    let dockMax = +this.appSettings.numberofcards_max;
+    let dockMin = +this.appSettings.numberofcards_min;
+    this.docknumber = dockMin;
+    if(dockMax > dockMin){
+      this.dockSize = this.RangeFill(dockMin,dockMax);
+    }else{
+      this.dockSize = this.RangeFill(3,10);
+    }
+    
   }
 
+  RangeFill(start, end) {
+    return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
+  }
+
+  StartGame(){
+    console.log(this.docknumber);
+  }
 }
