3c88b5e16e1363c8e448a2c63b8a987c5e7e03ee
Daniel Blasko <blaskoi@gmail.com>
2018-08-21 21:58:21 +0200

    Random image

--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Settings } from "../../services/settings.service";
+import { CardComponent } from '../card/card.component';
+import { Card } from '../../models/card.model';
 
 @Component({
   selector: 'app-landingpage',
@@ -9,9 +11,17 @@ import { Settings } from "../../services/settings.service";
 export class LandingpageComponent implements OnInit {
   deckSize: number[];
   decknumber: number;
+  deckNgFor: number[];
+  cardCollection: Card[] = [];
   showGameBoard = false;
+  imgSource: string;
+  faceUpCardNumber = 1;
+  isFaceUp = false;
+  @ViewChild('cardVar') cardVar: ElementRef<CardComponent>;
 
-  constructor(private appSettings: Settings) {}
+  constructor(private appSettings: Settings) {
+    
+  }
 
   ngOnInit() {
     let deckMax = +this.appSettings.numberofcards_max;
@@ -30,7 +40,31 @@ export class LandingpageComponent implements OnInit {
     return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
   }
 
+  RandomImg(): string{
+    let randomimage = this.appSettings.images[Math.floor(Math.random() * this.appSettings.images.length)];
+    let imageUrl = `../../../assets/images/cards/${randomimage}.png`;
+    return imageUrl;
+  }
+
   StartGame(){
+    console.log(this.decknumber);
+    this.deckNgFor = this.RangeFill(1,((this.decknumber)*2));
+    this.deckNgFor.map(x=>{
+      let card = {
+        img: this.RandomImg(),
+        status: 0
+      }
+      this.cardCollection.push(card);
+    })
     this.showGameBoard = true;
   }
+
+  FlipCard(cardVar){
+    console.log(cardVar.isEnd);
+    if(this.faceUpCardNumber < 3){
+      this.isFaceUp = true;
+      this.faceUpCardNumber++;
+    }
+  }
+
 }
---
+++ src/app/models/card.model.ts
@@ -0,0 +1,4 @@
+export class Card{
+    img:string;
+    status:number;
+}
